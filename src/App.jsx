24c85f68f91085daf305e569096e901febf5db6e
Luciano Garriga <lucianomgarriga@gmail.com>
2023-06-05 12:17:29 -0300

    Add 2 sections and styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,29 @@ export function App() {
   const format = (userName) => `@${userName}`;
 
   return (
-    <section className="follow-card-section">
-      <TwFollowCard format={format} userName="sindresorhus" isFollowing>
-        Sindre Sorhus
-      </TwFollowCard>
-      <TwFollowCard format={format} userName="pheralb" isFollowing={false}>
-        Pablo Hernandez
-      </TwFollowCard>
-      <TwFollowCard format={format} userName="joebiden" isFollowing={false}>
-        JOE 2020
-      </TwFollowCard>
-      <TwFollowCard format={format} userName="peterp" isFollowing>
-        Peter Parker
-      </TwFollowCard>
+    <section className="follow-card-section"> 
+    <div className="title-follow-box">
+        <span className="title-follow-txt">A quién seguir</span>
+    </div>
+    <div>
+        <TwFollowCard format={format} userName="sindresorhus" isFollowing>
+          Sindre Sorhus
+        </TwFollowCard>
+        <TwFollowCard format={format} userName="pheralb" isFollowing={false}>
+          Pablo Hernandez
+        </TwFollowCard>
+        <TwFollowCard format={format} userName="joebiden" isFollowing={false}>
+          JOE 2020
+        </TwFollowCard>
+        <TwFollowCard format={format} userName="peterp" isFollowing>
+          Peter Parker
+        </TwFollowCard> 
+    </div> 
+    <a href="https://twitter.com/home" className="tw-more-links-section">
+      <div className="title-follow-more">
+        <span className="tw-more-links-span">Mostrar mas</span>
+      </div>
+    </a> 
     </section>
   );
 }
--- src/TwFollowCard.jsx
+++ src/TwFollowCard.jsx
@@ -1,34 +1,25 @@
- 
-export const TwFollowCard = ({children, format, userName, name, isFollowing}) => { 
-  
-    console.log(isFollowing);
+export const TwFollowCard = ({ children, format, userName = 'unknown', isFollowing }) => {
+ const text = isFollowing ? 'Siguiendo' : 'Seguir'
 
-    
-    // const isFollowing = () => {
-    //     if (isFollowing == true) {
-    //         return "Seguir"
-    //     } else {
-    //         return false
-    //     }
-    // }
-
-    return (
+  return (
     <article className="tw-followCard">
-    <header className="tw-followCard-header">
-      <img
-        className="tw-followCard-avatar"
-        src={`https://unavatar.io/${userName}`}
-        alt="El avatar de Sindresorhus"
-      />
-      <div className="tw-followCard-info">
-        <strong>{name} {children}</strong>
-        <span className="tw-followCard-userName">{format(userName)}</span>
-      </div>
-    </header>
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src={`https://unavatar.io/${userName}`}
+          alt="El avatar de Sindresorhus"
+        />
+        <div className="tw-followCard-name">
+          <strong>{children}</strong>
+          <span className="tw-followCard-userName">{format(userName)}</span>
+        </div>
+      </header>
 
-    <aside>
-      <button className="tw-followCard-button">{isFollowing}</button>
-    </aside>
-  </article>
-  )
-}
+      <aside>
+        <button className="tw-followCard-button">
+          {text}
+        </button>
+      </aside>
+    </article>
+  );
+};
