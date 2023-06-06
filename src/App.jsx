9481339364b21a4b3dd8de41e07038fb43b5e2f0
Luciano Garriga <lucianomgarriga@gmail.com>
2023-06-06 10:46:54 -0300

    Add map users function

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
- 
 import "./App.css";
 import { TwFollowCard } from "./TwFollowCard";
 
-export function App() {  
- 
+export function App() {
+  const users = [
+    {
+      name: "Sindre Sorhus",
+      userName: "sindresorhus",
+      isFollowing: true,
+    },
+    {
+      name: "Pablo Hernandez",
+      userName: "pheralb",
+      isFollowing: false,
+    },
+    {
+      name: "JOE 2020",
+      userName: "joebiden",
+      isFollowing: true,
+    },
+    {
+      name: "Peter Parker",
+      userName: "peterp",
+      isFollowing: false,
+    },
+  ];
+
   return (
-    <section className="follow-card-section"> 
-    <div className="title-follow-box">
+    <section className="follow-card-section">
+      <div className="title-follow-box">
         <span className="title-follow-txt">A quién seguir</span>
-    </div>
-    <div>
-        <TwFollowCard userName="sindresorhus" initialIsFollowing={false}>
-          Sindre Sorhus
-        </TwFollowCard>
-        <TwFollowCard userName="pheralb" initialIsFollowing={false}>
-          Pablo Hernandez
-        </TwFollowCard>
-        <TwFollowCard userName="joebiden" initialIsFollowing={true}>
-          JOE 2020
-        </TwFollowCard>
-        <TwFollowCard userName="peterp" initialIsFollowing={false}>
-          Peter Parker
-        </TwFollowCard> 
-    </div>  
-    <a href="https://twitter.com/luchogarriga" className="tw-more-links-section">
-      <div className="title-follow-more">
-        <span className="tw-more-links-span">Mostrar más</span>
       </div>
-    </a> 
+      <div>
+        {users.map((user) => {
+          const { userName, name, isFollowing } = user
+
+          return (
+            <TwFollowCard 
+              key={userName}
+              userName={userName} 
+              initialIsFollowing={isFollowing}
+              >
+                {name}
+            </TwFollowCard>
+          );
+        })}
+      </div>
+      <a
+        href="https://twitter.com/i/connect_people"
+        className="tw-more-links-section"
+      >
+        <div className="title-follow-more">
+          <span className="tw-more-links-span">Mostrar más</span>
+        </div>
+      </a>
     </section>
   );
 }
